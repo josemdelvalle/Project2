1d7098e8dace2ac110cd1ed93d6316d322f8ecfd
serenesamb <serenesamb@gmail.com>
2021-05-12 02:09:47 -0400

    slight change

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -1,6 +1,6 @@
 def route(app):
     @app.route("/login/<username>/<password>", methods=["GET"])
     def users_login(username, password):
-        userlogin = UserService.login(username, password)
-        if userlogin:
-            return jsonify(logininfo.json()), 200
+        user_login = UserService.login(username, password)
+        if user_login:
+            return jsonify(user_login.json()), 200
