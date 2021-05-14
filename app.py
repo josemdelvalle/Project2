d5c51e44913e85eca6bb25153f5fd63ec5092b46
serenesamb <serenesamb@gmail.com>
2021-05-14 17:37:03 -0400

    orders service, dao, controller, model is added.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from controllers import front_controller as lc, product_controller as pc
+from controllers import front_controller as lc, product_controller as pc, orders_controller as oc
 from flask_cors import CORS
 
 app = Flask(__name__)
 
 lc.route(app)
 pc.route(app)
+oc.route(app)
 CORS(app, supports_credentials=True)
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
---
+++ controllers/orders_controller.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+
+from exceptions.resource_not_found import ResourceNotFound
+from services.orders_service_impl import OrrdersServiceImpl
+
+def route(app):
+    @app.route("/orders", methods=['GET'])
+    def get_orders():
+        try:
+            orders = OrrdersServiceImpl.get_orders()
+            return jsonify(orders), 200  # ok
+        except ValueError as e:
+            return "Not a valid ID", 400  # Bad Request
+        except ResourceNotFound as r:
+            return r.message, 404
