d0282af36cbe3e85daf4d3dcfb670b55ac58f644
JDelFig <55452601+JDelFig@users.noreply.github.com>
2021-05-14 23:01:06 -0400

    Updated storePage

--- templates/login.js
+++ templates/login.js
@@ -2,11 +2,11 @@ const btn = document.getElementById('loginButton');
 btn.addEventListener('click', (e) => {
     e.preventDefault(); // disable the refresh on the page when submit
     var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
+    xhr.withCredentials = true
     const username = document.getElementById('usernameInput').value;
     const password = document.getElementById('passwordInput').value;
     xhr.open("POST", "http://127.0.0.1:5000/login", true);
-    
+    ;
     console.log(username)
     console.log(password)
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -21,14 +21,17 @@ btn.addEventListener('click', (e) => {
             responseObject= JSON.parse(this.responseText);
             console.log(responseObject);
 
-            document.cookie = "username=John Doe";
-        //  window.location.href = "/storePage.html";
-        document.cookie = "username=John Doe; expires=Thu, 18 Dec 2021 12:00:00 UTC; path=/storePage.html";
         
+        
+        document.cookie = `firstName=${responseObject.firstName}; expires=Thu, 18 Dec 2021 12:00:00 UTC; path=/storePage.html`;
+        document.cookie = `lastName=${responseObject.lastName}; expires=Thu, 18 Dec 2021 12:00:00 UTC; path=/storePage.html`;
+        document.cookie = "Logedin=True; expires=Thu, 18 Dec 2021 12:00:00 UTC; path=/storePage.html";
+        window.location.href = "/storePage.html";
 
 
         }else{
             console.log(this.responseText);
+            // document.cookie = "isLogedin?=False; expires=Thu, 18 Dec 2021 12:00:00 UTC; path=/storePage.html";
         }
     }
     
--- templates/storePage.js
+++ templates/storePage.js
@@ -1,14 +1,31 @@
+const getCookie = (cookie_name) =>{
+    // Construct a RegExp object as to include the variable name
+    const re = new RegExp(`(?<=${cookie_name}=)[^;]*`);
+    try{
+      return document.cookie.match(re)[0];	// Will raise TypeError if cookie is not found
+    }catch{
+      return "this-cookie-doesn't-exist";
+    }
+  }
+  
+  firstName=getCookie("firstName");
+  isLogedin=getCookie("Logedin");
+  console.log(isLogedin);
+  
+//   if (isLogedin=="False");{
+//     window.location.href = "/login.html";
+//   }
 // Test
 //document.getElementById("container").innerHTML = "<p>This is a paragraph</p>"
 
-function createProductElement(srcImage, price, name){
-    newDiv = document.createElement("div");
-    document.getElementById("container").append(newDiv);
-    newDiv.innerHTML = "<image src=' " + srcImage + "'/> <p> name: " + 
-    name + "</p> <p>price: " + price;
-    // newDiv.id = "container" + id;
-    // document.getElementById("container" + id).innerHTML = 
-}
+// function createProductElement(srcImage, price, name){
+//     newDiv = document.createElement("div");
+//     document.getElementById("container").append(newDiv);
+//     newDiv.innerHTML = "<image src=' " + srcImage + "'/> <p> name: " + 
+//     name + "</p> <p>price: " + price;
+//     // newDiv.id = "container" + id;
+//     // document.getElementById("container" + id).innerHTML = 
+// }
 
 // Testing the ability to dynamically add product information into the website
 // createProductElement("", 8, "ice cream");
@@ -41,20 +58,20 @@ function createProductElement(srcImage, price, name){
 // }
 
 
-let xttp = XMLHttpRequest();
+// let xttp = XMLHttpRequest();
 
-xttp.onReadyStateChange() = function(){
-    if (this.readyState == 4 && this.status == 200){
-        console.log(this.responseText);
+// xttp.onReadyStateChange() = function(){
+//     if (this.readyState == 4 && this.status == 200){
+//         console.log(this.responseText);
 
-        let allProducts = JSON.parse(this.responseText);
-        for(let i = 0; i < allProducts.length; i++){
-            createProductElement(allProducts[i].srcImage, allProducts[i].price, allProducts[i].name);
-        }
-    }
-};
+//         let allProducts = JSON.parse(this.responseText);
+//         for(let i = 0; i < allProducts.length; i++){
+//             createProductElement(allProducts[i].srcImage, allProducts[i].price, allProducts[i].name);
+//         }
+//     }
+// };
 
-url = "localhost:5000/products";
-xttp.open("GET", url, true);
+// url = "localhost:5000/products";
+// xttp.open("GET", url, true);
 
-xttp.send();
+// xttp.send();
