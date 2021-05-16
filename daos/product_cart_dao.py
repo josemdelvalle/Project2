c1753c0c56674abdc64773cd90aaef78062df5c0
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2021-05-16 16:48:05 -0400

    Tested the ability to add products, get all products, and delete a product from the database

--- daos/product_cart_dao.py
+++ daos/product_cart_dao.py
@@ -13,3 +13,29 @@ class ProductCartDAO:
         record = cursor.fetchone()
         returned_product = Products(record[0], record[1], record[2]).json()
         return returned_product
+
+    @staticmethod
+    def delete_product_from_cart(product_id):
+        sql = "DELETE FROM product_cart WHERE product_id = %s RETURNING *"
+        cursor = connection.cursor()
+        cursor.execute(sql, [product_id])
+        connection.commit()
+        record = cursor.fetchone()
+        if record:
+            return Products(record[0], record[1], record[2]).json()
+        else:
+            return None
+
+    @staticmethod
+    def get_all_products():
+        sql = "SELECT * FROM product_cart"
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()
+        records = cursor.fetchall()
+        cart_list = []
+        for record in records:
+            product = Products(record[0], record[1], record[2])
+            cart_list.append(product.json())
+
+        return cart_list
--- sql_scripts/SereneDBScript.sql
+++ sql_scripts/SereneDBScript.sql
@@ -1,3 +1,4 @@
 drop table if exists product_cart;
 
-CREATE TABLE product_cart(product_id serial REFERENCES products(product_id), product_name varchar(50), product_price decimal(5,2));
+CREATE TABLE product_cart(product_id serial primary key REFERENCES products(
+product_id), product_name varchar(50), product_price decimal(5,2));
