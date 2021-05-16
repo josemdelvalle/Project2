ab320dbe6618f4dd2b322d63b97ee24ba6cb267e
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2021-05-16 12:19:17 -0400

    Started working on product overview page

---
+++ templates/productOverview.js
@@ -0,0 +1,11 @@
+var getCookie = (cookie_name) =>{
+    // Construct a RegExp object as to include the variable name
+    const re = new RegExp(`(?<=${cookie_name}=)[^;]*`);
+    try{
+      return document.cookie.match(re)[0];	// Will raise TypeError if cookie is not found
+    }catch{
+      return "this-cookie-doesn't-exist";
+    }
+  }
+
+document.getElementById("productName").innerHTML = getCookie("productId");
