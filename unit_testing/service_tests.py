f59a49ff6460e0adb12d8a0d2463e535caf4778d
Joaquin808 <42978361+Joaquin808@users.noreply.github.com>
2021-05-20 17:14:58 -0400

    Some more service tests

--- unit_testing/service_tests.py
+++ unit_testing/service_tests.py
@@ -1,10 +1,16 @@
 import unittest
 
+from exceptions.resource_not_found import ResourceNotFound
+from models.product_cart import ProductCart
+from models.products import Products
 from services.orders_service_impl import OrdersServiceImpl
+from services.product_cart_service import ProductCartService
+from services.product_service_Impl import ProductServiceImpl
 
 
 class ServiceTests(unittest.TestCase):
 
+    # Order Service
     @staticmethod
     def test_get_all_orders():
         try:
@@ -15,3 +21,30 @@ class ServiceTests(unittest.TestCase):
                 assert False
         except Exception:
             assert True
+
+    # Product Service
+    def test_get_product_by_id_success(self):
+        product = ProductServiceImpl.get_product_by_id(1)
+        self.assertEqual(type(product), type(Products()))
+
+    def test_get_product_by_id_failure(self):
+        try:
+            product = ProductServiceImpl.get_product_by_id(99)
+            self.assertEqual(type(product), type(Products()))
+        except ResourceNotFound as r:
+            assert True
+        else:
+            assert False
+
+    def test_get_products(self):
+        products_list = ProductServiceImpl.get_products()
+        product = products_list[9]
+        self.assertEqual(product["productId"], 10)
+
+    # Product Cart Service
+    def test_add_product_to_cart(self):
+        test_product_cart = ProductCart(1, 9, 1, "Banana Cream Pie", 6, 7)
+        returned_product_cart = ProductCartService.add_product_to_cart(test_product_cart)
+        self.assertEqual(type(returned_product_cart), type(test_product_cart))
+
+    
